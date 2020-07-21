bf472cb54f114c3ea7af769de0167697402efdbf
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2020-07-21 16:44:59 -0500

    Create checkemail.sh

---
+++ checkemail.sh
@@ -0,0 +1,18 @@
+if (( "$#" == 0 ))	#<1>
+then
+	printf 'Enter email address: '
+	read emailin
+else
+	emailin="$1"
+fi
+
+pwned=$(curl -s "https://haveibeenpwned.com/api/v2/breachedaccount/$emailin")	#<2>
+
+if [ "$pwned" == "" ]
+then
+	exit 1
+else
+	echo 'Account pwned in the following breaches:'
+	echo "$pwned" | grep -Po '"Name":".*?"' | cut -d':' -f2 | tr -d '\"'	#<3>
+	exit 0
+fi
